65f26c00ca17653b7f5c9b8d34fea0779a35ce2d
santiagopangue <101563614+santiagopangue@users.noreply.github.com>
2022-04-08 11:52:19 -0300

    SimulacroTerminado

---
+++ Assets/Scripts/Ej01.cs
@@ -0,0 +1,33 @@
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son 
+//iguales o no lo son.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej01 : MonoBehaviour
+{
+    public int valor1;
+    public int valor2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ingrese un valor "+ valor1);
+        Debug.Log("Ingrese el otro valor "+ valor2);
+
+        if (valor1 == valor2)
+        {
+            Debug.Log("Los valores son iguales");
+        }
+        else
+        {
+            Debug.Log("Los valores son distintos");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej02.cs
@@ -0,0 +1,49 @@
+﻿//2.Crear las variables necesarias para ingresar por Inspector el precio de 3 productos 
+//y un monto de dinero disponible. Comunicar si la suma de los mismos supera o no el monto de 
+//dinero disponible. Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej02 : MonoBehaviour
+{
+    public float producto1;
+    public float producto2;
+    public float producto3;
+    public float monto;
+    float productoTotal;
+    float dineroExtra;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ingresar el precio del primer producto "+ producto1);
+        Debug.Log("Ingresar el precio del segundo producto "+ producto2);
+        Debug.Log("Ingresar el precio del tercer producto "+ producto3);
+        Debug.Log("Ingresar el monto " + monto);
+        productoTotal = producto1 + producto2 + producto3;
+
+        if (productoTotal > monto)
+        {
+            Debug.Log("Su monto de dinero dispobible es menor al valor de los productos");
+            dineroExtra = productoTotal - monto;
+            Debug.Log("Falta $" + dineroExtra);
+        }
+        else if (productoTotal < monto)
+        {
+            Debug.Log("Su monto de dinero disponible es mayor al valor de los productos");
+            dineroExtra = monto - productoTotal;
+            Debug.Log("Sobra $" + dineroExtra);
+        }
+        else if (productoTotal == monto)
+        {
+            Debug.Log("Alcanzo el dinero exacto");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
